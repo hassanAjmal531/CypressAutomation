0c6648a0f09bea60ecc48b6dbfd7d685e4283458
Muhammad Hassan Ajmal <hassan.ajmal@venturedive.com>
2023-08-26 20:12:49 +0500

    Project setup

---
+++ cypress/e2e/DemoQaTests.cy.js
@@ -0,0 +1,19 @@
+
+describe('Automating Tasks On Demo QA Website', () => {
+
+  beforeEach(()=>{
+    cy.visit("/")
+  })
+  
+  it("Demoqa - Forms - Practice forms - Verify modal represents same image after form submission",()=>{
+
+  })
+
+  it("Demoqa - Interactions - Performing interactions",()=>{
+
+  })
+
+  it("Demoqa - Bookstore - verify the api response is correct",()=>{
+    
+  })
+})
---
+++ cypress/fixtures/formData.js
@@ -0,0 +1,12 @@
+module.exports = {
+    firstName : "Cowlar",
+    lastName : "Developer",
+    email : "qaengineer@cowlar.com",
+    gender: "male",
+    mobileNo : "0123456789",
+    subjects: "Computer Science",
+    hobbies: "Music",
+    currentAddress: "Address 1",
+    state: "NCR",
+    city: "Delhi"
+}
